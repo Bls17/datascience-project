dce6fa8cec8568f46399d56df42526a5619d00f5
Badara Diallo <badaradiallo@Host-001.lan>
2025-06-21 00:49:33 +0200

    Data Validation

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import (
     DataIngestionTrainingPipeline,
 )
+from src.datascience.pipeline.data_validation_pipeline import (
+    DataValidationTrainingPipeline,
+)
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -15,3 +18,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise (e)
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
