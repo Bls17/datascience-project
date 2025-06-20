71517424226a6f328712ae33d87415f008a17b39
Badara Diallo <badaradiallo@Host-001.lan>
2025-06-20 20:59:55 +0200

    Data Ingestion

---
+++ src/datascience/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(
+    "/Users/badaradiallo/Desktop/DataScience_Projetc/config/config.yaml"
+)
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
