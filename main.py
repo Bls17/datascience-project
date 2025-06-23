dda6f6541dbee9d54f95f04b4d39ed91a392253e
Badara Diallo <badaradiallo@Host-001.lan>
2025-06-23 12:39:21 +0200

    Data Transformation

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from src.datascience.pipeline.data_ingestion_pipeline import (
 from src.datascience.pipeline.data_validation_pipeline import (
     DataValidationTrainingPipeline,
 )
+from src.datascience.pipeline.data_transformation_pipeline import (
+    DataTransformationTrainingPipeline,
+)
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -25,6 +28,16 @@ try:
     logger.info(f"Stage {STAGE_NAME} started")
     data_validation = DataValidationTrainingPipeline()
     data_validation.initiate_data_validation()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} started")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
     logger.info(f"Stage {STAGE_NAME} completed")
 except Exception as e:
     logger.exception(e)
--- src/datascience/pipeline/data_ingestion_pipeline.py
+++ src/datascience/pipeline/data_ingestion_pipeline.py
@@ -15,16 +15,3 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zipfile()
-
-
-if __name__ == "__main__":
-    try:
-        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.initiate_data_ingestion()
-        logger.info(
-            f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<\n\nx==============x"
-        )
-    except Exception as e:
-        logger.exception(e)
-        raise (e)
